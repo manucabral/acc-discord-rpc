20329953ff2575fd00343620b30e3d5ae67eb1f4
manucabral <mcabral@frba.utn.edu.ar>
2022-01-22 23:40:20 -0300

    Update constants.py

--- accrpc/constants.py
+++ accrpc/constants.py
@@ -2,7 +2,7 @@ TITLE = 'Assetto Corsa Competizione - Discord Rich Presence'
 AUTHOR = 'Manuel Cabral'
 GITHUB = 'https://github.com/manucabral'
 REPO_URL = "https://api.github.com/repos/manucabral/acc-discord-rpc/releases"
-VERSION = '0.1'
+VERSION = '0.2'
 GAME = 'Assetto Corsa Competizione'
 
 LANGUAGE = {
@@ -44,43 +44,43 @@ SESSION = {
 }
 
 CAR_MODEL = {
-    'amr_v12_vantage_gt3': 'Aston Martin Vantage V12 GT3 2013',
-    'audi_r8_lms': 'Audi R8 LMS 2015',
-    'bentley_continental_gt3_2016': 'Bentley Continental GT3 2015',
-    'bentley_continental_gt3_2018': 'Bentley Continental GT3 2018',
-    'bmw_m6_gt3': 'BMW M6 GT3 2017',
-    'jaguar_g3': 'Emil Frey Jaguar G3 2012',
-    'ferrari_488_gt3': 'Ferrari 488 GT3 2018',
-    'honda_nsx_gt3': 'Honda NSX GT3 2017',
-    'lamborghini_gallardo_rex': 'Lamborghini Gallardo G3 Reiter 2017',
-    'lamborghini_huracan_gt3': 'Lamborghini Huracan GT3 2015',
-    'lexus_rc_f_gt3': 'Lexus RCF GT3 2016',
-    'mclaren_650s_gt3': 'McLaren 650S GT3 2015',
-    'mercedes_amg_gt3': 'Mercedes AMG GT3 2015',
-    'nissan_gt_r_gt3_2017': 'Nissan GTR Nismo GT3 2015',
-    'nissan_gt_r_gt3_2018': 'Nissan GTR Nismo GT3 2018',
-    'porsche_991_gt3_r': 'Porsche 991 GT3 R 2018',
-    'amr_v8_vantage_gt3': 'Aston Martin V8 Vantage GT3 2019',
-    'audi_r8_lms_evo': 'Audi R8 LMS Evo 2019',
-    'honda_nsx_gt3_evo': 'Honda NSX GT3 Evo 2019',
-    'lamborghini_huracan_gt3_evo': 'Lamborghini Huracan GT3 EVO 2019',
-    'mclaren_720s_gt3': 'McLaren 720S GT3 2019',
-    'porsche_991ii_gt3_r': 'Porsche 911 II GT3 R 2019',
-    'ferrari_488_gt3_evo': 'Ferrari 488 GT3 Evo 2020',
-    'mercedes_amg_gt3_evo': 'Mercedes AMG GT3 Evo 2020',
-    'alpine_a110_gt4': 'Alpine A110 GT4 2018',
-    'amr_v8_vantage_gt4': 'ston Martin Vantage AMR GT4 2018',
-    'audi_r8_gt4': 'Audi R8 LMS GT4 2016',
-    'bmw_m4_gt4': 'BMW M4 GT4 2018',
-    'chevrolet_camaro_gt4r': 'Chevrolet Camaro GT4 R 2017',
-    'ginetta_g55_gt4': 'Ginetta G55 GT4 2012',
-    'ktm_xbow_gt4': 'Ktm Xbow GT4 2016',
-    'maserati_mc_gt4': 'Maserati Gran Turismo MC GT4 2016',
-    'mclaren_570s_gt4': 'McLaren 570s GT4 2016',
-    'mercedes_amg_gt4': 'Mercedes AMG GT4 2016',
-    'porsche_718_cayman_gt4_mr': 'Porsche 718 Cayman GT4 MR 2019',
-    'porsche_991ii_gt3_cup': 'Porsche9 91 II GT3 Cup 2017',
-    'lamborghini_huracan_st': 'Lamborghini Huracan ST 2015'
+    'amr_v12_vantage_gt3': ('Aston Martin Vantage V12 GT3 2013', 'aston_martin'),
+    'audi_r8_lms': ('Audi R8 LMS 2015', 'audi'),
+    'bentley_continental_gt3_2016': ('Bentley Continental GT3 2015', 'bentley'),
+    'bentley_continental_gt3_2018': ('Bentley Continental GT3 2018', 'bentley'),
+    'bmw_m6_gt3': ('BMW M6 GT3 2017', 'bmw'),
+    'jaguar_g3': ('Emil Frey Jaguar G3 2012', 'jaguar'),
+    'ferrari_488_gt3': ('Ferrari 488 GT3 2018', 'ferrari'),
+    'honda_nsx_gt3': ('Honda NSX GT3 2017', 'honda'),
+    'lamborghini_gallardo_rex': ('Lamborghini Gallardo G3 Reiter 2017', 'lamborghini'),
+    'lamborghini_huracan_gt3': ('Lamborghini Huracan GT3 2015', 'lamborghini'),
+    'lexus_rc_f_gt3': ('Lexus RCF GT3 2016', 'lexus'),
+    'mclaren_650s_gt3': ('McLaren 650S GT3 2015', 'mclaren'),
+    'mercedes_amg_gt3': ('Mercedes AMG GT3 2015', 'mercedes'),
+    'nissan_gt_r_gt3_2017': ('Nissan GTR Nismo GT3 2015', 'nissan'),
+    'nissan_gt_r_gt3_2018': ('Nissan GTR Nismo GT3 2018', 'nissan'),
+    'porsche_991_gt3_r': ('Porsche 991 GT3 R 2018', 'porsche'),
+    'amr_v8_vantage_gt3': ('Aston Martin V8 Vantage GT3 2019', 'aston_martin'),
+    'audi_r8_lms_evo': ('Audi R8 LMS Evo 2019', 'audi'),
+    'honda_nsx_gt3_evo': ('Honda NSX GT3 Evo 2019', 'honda'),
+    'lamborghini_huracan_gt3_evo': ('Lamborghini Huracan GT3 EVO 2019', 'lamborghini'),
+    'mclaren_720s_gt3': ('McLaren 720S GT3 2019', 'mclaren'),
+    'porsche_991ii_gt3_r': ('Porsche 911 II GT3 R 2019', 'porsche'),
+    'ferrari_488_gt3_evo': ('Ferrari 488 GT3 Evo 2020', 'ferrari'),
+    'mercedes_amg_gt3_evo': ('Mercedes AMG GT3 Evo 2020', 'mercedes'),
+    'alpine_a110_gt4': ('Alpine A110 GT4 2018', 'alpine'),
+    'amr_v8_vantage_gt4': ('Aston Martin Vantage AMR GT4 2018', 'aston_martin'),
+    'audi_r8_gt4': ('Audi R8 LMS GT4 2016', 'audi'),
+    'bmw_m4_gt4': ('BMW M4 GT4 2018', 'bmw'),
+    'chevrolet_camaro_gt4r': ('Chevrolet Camaro GT4 R 2017', 'chevrolet'),
+    'ginetta_g55_gt4': ('Ginetta G55 GT4 2012', 'ginetta'),
+    'ktm_xbow_gt4': ('Ktm Xbow GT4 2016', 'ktm'),
+    'maserati_mc_gt4': ('Maserati Gran Turismo MC GT4 2016', 'maserati'),
+    'mclaren_570s_gt4': ('McLaren 570s GT4 2016', 'mclaren'),
+    'mercedes_amg_gt4': ('Mercedes AMG GT4 2016', 'mercedes'),
+    'porsche_718_cayman_gt4_mr': ('Porsche 718 Cayman GT4 MR 2019', 'porsche'),
+    'porsche_991ii_gt3_cup': ('Porsche9 91 II GT3 Cup 2017', 'porsche'),
+    'lamborghini_huracan_st': ('Lamborghini Huracan ST 2015', 'lamborghini')
 }
 
 MSG = [
