4bc64eb054149c8e740f25ffd1bf1d94a8b75a18
Manu <mcabral@frba.utn.edu.ar>
2023-06-08 00:23:01 -0300

    feat: add __init__ for module

---
+++ accrpc/__init__.py
@@ -0,0 +1,7 @@
+from .constants import TITLE, VERSION, AUTHOR
+from .core import Core
+
+__title__ = TITLE
+__version__ = VERSION
+__author__ = AUTHOR
+__all__ = ["__title__", "__version__", "__author__", "__license__", "Core"]
